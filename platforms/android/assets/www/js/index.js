ca46b2bc5df05055c073c5d5280f3c6853f309cf
Teague Lander <tlander121@hotmail.com>
2016-03-22 08:27:21 -0700

    Squares & drag and drop working

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,73 +1,20 @@
 //CONSTANTS
 var GAME_SCALE = 1;
+var PIECE_SCALE = 1.3;
+var STAND_SIZE = 480;
+var OFF_X = 1;
+var OFF_Y = 1;
 
 var chessStage = new createjs.Stage("chessCanvas");
 var preloadChess;
 var manifestChess;
+var pieceLocationIndex = [];
+var boardOfObjects = [];
 
 //Resource namespace
 
-
-
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
-
-function init() {
-		
-	var stage = new createjs.Stage("demoCanvas");
-	var circle = new createjs.Shape();
-	circle.graphics.beginFill("red").drawCircle(0,0,50);
-	circle.x = 100;
-	circle.y = 100;
-	stage.addChild(circle);
-		
-	//stage.update();
-	
-	createjs.Tween.get(circle)
-		.to({x:400}, 2000, createjs.Ease.getPowInOut(4))
-		.to({alpha:0, y:75}, 500, createjs.Ease.getPowInOut(2))
-		.to({alpha: 0, y:125}, 100)
-		.to({alpha: 1, y:100}, 500, createjs.Ease.getPowInOut(2))
-		.to({x:100}, 800, createjs.Ease.getPowInOut(2))
-		;
-			
-	createjs.Ticker.setFPS(60);
-	createjs.Ticker.addEventListener("tick",stage);
-			
-}
-
 function initChess() {
+	setupViewSize();
 	var chessStage = new createjs.Stage("chessCanvas");
 	initGame();
 	setupCanvasBoardPieces();
@@ -76,25 +23,63 @@ function initChess() {
 
 function setupCanvasBoardPieces() {
 	var boardDrawing = new createjs.Bitmap("./img/board_8x8.svg");//(imgBoard);
-	boardDrawing.ScaleX = boardDrawing.ScaleY = GAME_SCALE;
 	chessStage.addChild(boardDrawing);
+	
+	//Tracks the display objects
+	boardOfObjects = new Array(8);
+	for (var i = 0; i < boardOfObjects.length; i++) {
+        boardOfObjects[i] = new Array(8);
+        for (var j = 0; j < boardOfObjects[i].length; j++) {
+            boardOfObjects[i][j] = null;
+        }
+    }
 	
 	squareHeight = document.getElementById('chessCanvas').width / 8;
 	for (var i = 0; i < board.length; i++) {
 		for (var j = 0; j < board[i].length; j++) {
 			if (board[i][j]) {
 				var piece = new createjs.Bitmap("./img/pieces_svg/" + lookupPieceResource(board[i][j]));
-				piece.x = 1+j*60; piece.scaleX = 1.30*GAME_SCALE;
-				piece.y = 1+(7-i)*60; piece.scaleY = 1.30*GAME_SCALE;
+				pieceLocationIndex[piece.id] = board[i][j];
+				piece.on("pressmove", piecePressed);
+				piece.on("pressup", pieceReleased);
+				piece.x = OFF_X+j*60; piece.scaleX = PIECE_SCALE;
+				piece.y = OFF_Y+(7-i)*60; piece.scaleY = PIECE_SCALE;
 				chessStage.addChild(piece);
+				boardOfObjects[i][j] = piece;
 			}
 		}
 	}
 	
-	createjs.Ticker.setFPS(20);
+	createjs.Ticker.setFPS(30);
 	createjs.Ticker.addEventListener("tick",chessStage);
-	
 }
+
+function piecePressed(evt) {
+	chessStage.setChildIndex( evt.target, chessStage.getNumChildren()-1);
+	evt.target.x = evt.stageX/GAME_SCALE-evt.target.getBounds().width/2;
+	evt.target.y = evt.stageY/GAME_SCALE-evt.target.getBounds().height/2;
+}
+
+function pieceReleased(evt) {
+	board_piece = pieceLocationIndex[evt.target.id];
+	var j = Math.floor((evt.stageX/GAME_SCALE)/60);
+	var i = 7-Math.floor((evt.stageY/GAME_SCALE)/60);
+	var orgX = board_piece.squareX;
+	var orgY = board_piece.squareY;
+	if (checkMoveLegality(orgY,orgX,i,j)) {
+		movePiece(orgY,orgX,i,j);
+		if (boardOfObjects[i][j]) { //Removes the piece below
+			chessStage.removeChild(boardOfObjects[i][j]);
+		}
+		boardOfObjects[i][j] = evt.target;
+		boardOfObjects[orgY][orgX] = null;
+	}
+	evt.target.x = OFF_X+board_piece.squareX*60; //snap X
+	evt.target.y = OFF_Y+(7-board_piece.squareY)*60; //snap Y
+	//evt.target.x = OFF_X+j*60;
+	//evt.target.y = OFF_Y+(7-i)*60;
+}
+
 
 var soundID = "groove";
 function playSound() {
@@ -113,6 +98,16 @@ function lookupPieceResource(piece) {
 	else if (piece.pieceType == 'B') { return color + "_bishop.svg"; }
 	else if (piece.pieceType == 'Q') { return color + "_queen.svg"; }
 	else { return color + "_king.svg"; }
+}
+
+function setupViewSize() {
+	var canvas = document.getElementById("chessCanvas");
+	var maxWindowSize = Math.min(window.innerWidth,window.innerHeight);
+	canvas.width = maxWindowSize;
+	canvas.height = maxWindowSize;
+	GAME_SCALE = maxWindowSize/480;
+	chessStage.scaleX = GAME_SCALE;
+	chessStage.scaleY = GAME_SCALE;
 }
 
 /*function loadResources() {
--- www/js/chess_match.js
+++ www/js/chess_match.js
@@ -31,15 +31,38 @@ function parseCommand() {
 	}
 }
 
-function movePiece(r0,f0,r1,f1) {
-    board[r1][f1] = board[r0][f0];
-    board[r0][f0] = null;
-    printBoard();
+function checkMoveLegality(r0,f0,r1,f1) {
+	console.log("Legality check:");
+	if (r0 > 7 || f0 > 7 || r1 > 7 || f1 > 7 || r0 < 0 || f0 < 0 || r1 < 0 || f1 < 0) {
+		console.log("Outside bounds");
+		return false;
+	}else if (r0 == r1 && f0 == f1) {
+		console.log("No move");
+		return false;
+	}else if (board[r1][f1]) {
+		if (board[r0][f0].color == board[r1][f1].color) {
+			console.log("Can't capture your own piece!");
+			return false;
+		}
+	}
+	return true;
 }
 
-function gamePiece(pieceType,color) {
+function movePiece(r0,f0,r1,f1) {
+	//if (checkLegality(r0,f0,r1,f1)) {
+		board[r1][f1] = board[r0][f0];
+		board[r1][f1].squareX = f1;
+		board[r1][f1].squareY = r1;
+		board[r0][f0] = null;
+		printBoard();
+	//}
+}
+
+function gamePiece(pieceType,color,sy,sx) {
     this.color = color;
     this.pieceType = pieceType;
+	this.squareX = sx;
+	this.squareY = sy;
 }
 
 function setupBoard() {
@@ -53,28 +76,28 @@ function setupBoard() {
     }
     
     //Setup the pieces in the 2d array
-    board[0][0] = new gamePiece("R","W");
-    board[0][1] = new gamePiece("N","W");
-    board[0][2] = new gamePiece("B","W");
-    board[0][3] = new gamePiece("Q","W");
-    board[0][4] = new gamePiece("K","W");
-    board[0][5] = new gamePiece("B","W");
-    board[0][6] = new gamePiece("N","W");
-    board[0][7] = new gamePiece("R","W");
+    board[0][0] = new gamePiece("R","W",0,0);
+    board[0][1] = new gamePiece("N","W",0,1);
+    board[0][2] = new gamePiece("B","W",0,2);
+    board[0][3] = new gamePiece("Q","W",0,3);
+    board[0][4] = new gamePiece("K","W",0,4);
+    board[0][5] = new gamePiece("B","W",0,5);
+    board[0][6] = new gamePiece("N","W",0,6);
+    board[0][7] = new gamePiece("R","W",0,7);
     for (var i = 0; i < 8; i++) {
-        board[1][i] = new gamePiece("P","W");
+        board[1][i] = new gamePiece("P","W",1,i);
     }
     for (var i = 0; i < 8; i++) {
-        board[6][i] = new gamePiece("P","B");
+        board[6][i] = new gamePiece("P","B",6,i);
     }
-    board[7][0] = new gamePiece("R","B");
-    board[7][1] = new gamePiece("N","B");
-    board[7][2] = new gamePiece("B","B");
-    board[7][3] = new gamePiece("Q","B");
-    board[7][4] = new gamePiece("K","B");
-    board[7][5] = new gamePiece("B","B");
-    board[7][6] = new gamePiece("N","B");
-    board[7][7] = new gamePiece("R","B");
+    board[7][0] = new gamePiece("R","B",7,0);
+    board[7][1] = new gamePiece("N","B",7,1);
+    board[7][2] = new gamePiece("B","B",7,2);
+    board[7][3] = new gamePiece("Q","B",7,3);
+    board[7][4] = new gamePiece("K","B",7,4);
+    board[7][5] = new gamePiece("B","B",7,5);
+    board[7][6] = new gamePiece("N","B",7,6);
+    board[7][7] = new gamePiece("R","B",7,7);
 }
 
 function printBoard() { //TEST FUNCTION
